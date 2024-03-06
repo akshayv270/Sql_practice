72ce720abf0667caa2630195e6e20008723dc9c1
akshayv270 <akshayv270@gmail.com>
2024-03-06 17:40:30 +0530

    Add files via upload

---
+++ Alter_Command_050324.sql
@@ -0,0 +1,20 @@
+----To add new Column in table---
+Alter table tbl_Student
+Add fees varchar(10)
+
+-----To delete Column in table----
+
+Alter table tbl_student
+Drop column Student_Gender
+
+----TO change datatype columan------
+
+Alter table tbl_student
+Alter column Student_Gender int
+
+----To change Column Name------
+
+Alter table tbl_Student
+Alter column  fees numeric(10,2)
+
+
